261a11487eaee817cf33a995f314136b55d62baa
Joshua Nguyen's Mac <frostorange32@gmail.com>
2022-04-08 13:17:20 -0400

    start

---
+++ sick_nasty_trader.py
@@ -0,0 +1,9 @@
+# Code to do some sick nasty HFT
+import numpy as np
+
+# Model stock market movement as a mass-spring-dampener system on a moving base.
+# Apply ML/AI techniques to find suitable coefficients of the mass spring dampener.
+# Possibly have some sort of volume (trading, issued, etc.) as mass?
+
+
+
